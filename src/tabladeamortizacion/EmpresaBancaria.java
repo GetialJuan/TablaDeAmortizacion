676224f47337261aa31ededcb5fc28db90062bb6
Mauricio Munoz <mauricio.munoz.gutierrez@correounivalle.edu.co>
2022-06-30 16:04:43 -0500

    meses y monto

--- src/tabladeamortizacion/EmpresaBancaria.java
+++ src/tabladeamortizacion/EmpresaBancaria.java
@@ -4,10 +4,54 @@
  */
 package tabladeamortizacion;
 
+import java.util.Scanner;
+
 /**
  *
  * @author invitado
  */
 public class EmpresaBancaria {
+    private float monto;
+    private int meses;
+    private float tasa;
+    private float cuota;
+    private float saldo;
     
+    public EmpresaBancaria(){
+        monto = 0;
+        meses = 0;
+        tasa = 0;
+        cuota = 0;
+        saldo = 0;
+    }
+    
+    public void setMonto(){
+        Scanner sc = new Scanner(System.in);
+        try{
+            monto = Float.parseFloat(sc.nextLine());
+        }catch(NumberFormatException e){
+            System.out.println("Introduce un valor valido.");
+            setMonto();
+        }
+    }
+    
+    public void setMeses(){
+        Scanner sc = new Scanner(System.in);
+        while(Integer.parseInt(sc.nextLine()) < 1){
+            System.out.println("Introduce un valor mayor o igual a 1");
+            sc = new Scanner(System.in);
+        }
+        try{
+            meses = Integer.parseInt(sc.nextLine());
+        }catch(NumberFormatException e){
+            System.out.println("Introduce un valor valido mayor o igual a 1.");
+            setMeses();
+        }
+    }
+    
+    public void setTasa(){
+        if(monto < 1500000 && meses < 12){
+            tasa = (float)(2.5/100);
+        }
+    }
 }
